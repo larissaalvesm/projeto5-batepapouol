cced15ea9b20a4fefc827830de32a3385935f32e
larissa <larissaalves1102@gmail.com>
2023-01-20 16:31:23 -0300

    Implementação do chat sem o filtro de mensagens privadas

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let nome;
 const intervaloChecagemConexao = 5000;
+const intervaloCarregarMensagens = 3000;
+let mensagens = [];
 
 function entrarNaSala() {
     nome = prompt("Qual seu nome?");
@@ -14,6 +16,8 @@ function entrarNaSala() {
 
 function tratarSucesso(resposta) {
     const status = resposta.status;
+
+    carregarMensagens();
 }
 
 function tratarErro(erro) {
@@ -47,6 +51,62 @@ function desconectou(erro) {
     console.log("Não foi possível manter o usuário conectado.")
 }
 
+function carregarMensagens() {
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promise.then(mensagensCarregadas);
+    promise.catch(erroCarregarMensagens);
+
+    setTimeout(carregarMensagens, intervaloCarregarMensagens);
+}
+
+function mensagensCarregadas(resposta) {
+
+    mensagens = resposta.data;
+
+    exibirMensagens();
+
+}
+
+function erroCarregarMensagens() {
+    console.log(erro);
+}
+
+function exibirMensagens() {
+    const lista = document.querySelector('.mensagens');
+
+    lista.innerHTML = '';
+
+    for (let i = 0; i < mensagens.length; i++) {
+
+        if (mensagens[i].type === "status") {
+            let template = `
+        <div class="${mensagens[i].type}" data-test="message">
+                <p>
+                    <span class="hora">(${mensagens[i].time})</span>
+                    <span class="nome">${mensagens[i].from} </span> ${mensagens[i].text}
+                </p>
+        </div>
+        `
+            lista.innerHTML = lista.innerHTML + template;
+
+        } else {
+
+            let template = `
+        <div class="${mensagens[i].type}" data-test="message">
+                <p>
+                    <span class="hora">(${mensagens[i].time})</span>
+                    <span class="nome">${mensagens[i].from} </span> para <span class="nome">${mensagens[i].to} </span>: ${mensagens[i].text}
+                </p>
+        </div>
+        `
+            lista.innerHTML = lista.innerHTML + template;
+        }
+
+    }
+
+    document.querySelector(".mensagens").lastChild.scrollIntoView(true);
+}
 
 entrarNaSala();
+
 manterConexao();
